2348fc8c78518d0de05ae75d9b213047cb5c2dc2
Eduardo Candido <eduardocandidopro@gmail.com>
2020-11-03 21:24:13 -0300

    projeto prático Jasmine

---
+++ src/Consulta.js
@@ -0,0 +1,29 @@
+function Consulta(paciente, procedimentos, particular, retorno, data) {
+
+    var clazz = {
+        preco : function() {
+            if(retorno) return 0;
+
+            var precoFinal = 0;
+
+            procedimentos.forEach(function(procedimento) {
+                if("raio-x" == procedimento) precoFinal += 55;
+                else if("gesso" == procedimento) precoFinal += 32;
+                else precoFinal += 25;
+            });
+
+            if(particular) precoFinal *= 2;
+
+            return precoFinal;
+        },
+
+        getNome: ()=>{ return paciente },
+        getProcedimentos: ()=> {return procedimentos},
+        isParticular: ()=>{return particular},
+        isRetorno: ()=>{return retorno},
+        getData: ()=>{return data}
+
+    }
+
+    return clazz;
+};
